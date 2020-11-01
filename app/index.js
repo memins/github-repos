7348a7202eaab9405073fbcecde06689a68bbdce
Emin Sahin <m.emins@yahoo.com>
2020-11-02 00:13:14 +0100

    added subnav ( popular repos )

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Popular from "./components/Popular";
 
 class App extends React.Component {
   render() {
-    return <div>Hello mes!</div>;
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
   }
 }
 
