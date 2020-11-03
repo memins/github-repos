e7e6282d9b1260ffd6f22c88f39419cc458dfc95
Emin Sahin <m.emins@yahoo.com>
2020-11-03 23:58:28 +0100

    added subnavbar on Popular page

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,57 @@
 import React from "react";
+import PropTypes from 'prop-types'
+
+function LanguagesNav({selected, onUpdateLanguage}){
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+  return(
+    <ul className='flex-center'>
+    {languages.map((language)=>(
+      <li key={language}>
+      <button
+        className='btn-clear nav-link'
+        style={language === selected ? {color: 'rgb(187,46,31'} : null}
+        onClick={()=> onUpdateLanguage(language)}>
+          {language}
+        </button>
+      </li>
+      ))}
+    </ul>
+  )
+}
+
+LanguagesNav.propTypes = {
+  selected : PropTypes.string.isRequired,
+  onUpdateLanguage : PropTypes.func.isRequired
+}
 
 export default class Popular extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(selectedLanguage){
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render() {
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    const { selectedLanguage } = this.state
 
     return (
-      <ul className="flex-center">
-        {languages.map((language) => (
-          <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
-          </li>
-        ))}
-      </ul>
+     <React.Fragment>
+       <LanguagesNav
+        selected={selectedLanguage}
+        onUpdateLanguage={this.updateLanguage}
+       />
+     </React.Fragment>
     );
   }
 }
