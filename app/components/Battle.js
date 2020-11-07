670a15e2aee34663217f719cb0dd5debf3b4eaab
Emin Sahin <m.emins@yahoo.com>
2020-11-07 23:31:05 +0100

    added Reusable Card

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,12 @@
-import PropTypes from "prop-types";
 import React from "react";
-import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import {
+  FaUserFriends,
+  FaFighterJet,
+  FaTrophy,
+  FaTimesCircle,
+} from "react-icons/fa";
+import PropTypes from "prop-types";
+import Results from "./Result";
 
 function Instructions() {
   return (
@@ -11,7 +17,7 @@ function Instructions() {
           <h3 className="header-sm">Enter two Github users</h3>
           <FaUserFriends
             className="bg-light"
-            color="rgb(255,191,116)"
+            color="rgb(255, 191, 116)"
             size={140}
           />
         </li>
@@ -21,7 +27,7 @@ function Instructions() {
         </li>
         <li>
           <h3 className="header-sm">See the winners</h3>
-          <FaTrophy className="bg-light" color="rgb(255,215,0)" size={140} />
+          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140} />
         </li>
       </ol>
     </div>
@@ -83,6 +89,35 @@ PlayerInput.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
+function PlayerPreview({ username, onReset, label }) {
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className="row bg-light">
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
 export default class Battle extends React.Component {
   constructor(props) {
     super(props);
@@ -90,38 +125,70 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(id, player) {
     this.setState({
       [id]: player,
     });
   }
+  handleReset(id) {
+    this.setState({
+      [id]: null,
+    });
+  }
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
 
     return (
       <React.Fragment>
         <Instructions />
+
         <div className="players-container">
           <h1 className="center-text header-lg">Players</h1>
           <div className="row space-around">
-            {playerOne === null && (
+            {playerOne === null ? (
               <PlayerInput
                 label="Player One"
                 onSubmit={(player) => this.handleSubmit("playerOne", player)}
               />
+            ) : (
+              <PlayerPreview
+                username={playerOne}
+                label="Player One"
+                onReset={() => this.handleReset("playerOne")}
+              />
             )}
 
-            {playerTwo === null && (
+            {playerTwo === null ? (
               <PlayerInput
                 label="Player Two"
                 onSubmit={(player) => this.handleSubmit("playerTwo", player)}
               />
+            ) : (
+              <PlayerPreview
+                username={playerTwo}
+                label="Player Two"
+                onReset={() => this.handleReset("playerTwo")}
+              />
             )}
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className="btn dark-btn btn-space"
+              onClick={() => this.setState({ battle: true })}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     );
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,13 +1,86 @@
-export function fetchPopularRepos (language) {
-  const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+import { func } from "prop-types";
 
-  return fetch(endpoint)
-    .then((res)=> res.json())
-    .then((data) => {
-      if (!data.items){
-        throw new Error(data.message)
+const id = "441c36bb3140ee8e0f01"; //"YOUR_CLIENT_ID"
+const sec = "af8acdfbafdc172bcd561b65d231bfa5cc5522ee"; //"YOUR_SECRET_ID"
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function getErrorMsg(message, username) {
+  if (message === "Not Found") {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
+function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username));
       }
 
-      return data.items
+      return profile;
+    });
+}
+
+function getRepos(username) {
+  return fetch(
+    `https://api.github.com/users/${username}/repos${params}&per_page=100`
+  )
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username));
+      }
+
+      return repos;
+    });
+}
+
+function getStarCount(repos) {
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarCount(repos);
+}
+
+function getUserData(player) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
     })
+  );
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+  return Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1]),
+  ]).then((results) => sortPlayers(results));
+}
+
+export function fetchPopularRepos(language) {
+  const endpoint = window.encodeURI(
+    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  );
+
+  return fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.items) {
+        throw new Error(data.message);
+      }
+
+      return data.items;
+    });
 }
