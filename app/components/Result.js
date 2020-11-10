33c6866516750cc5c20b0034fd35d76b589990d4
Emin Sahin <m.emins@yahoo.com>
2020-11-10 23:13:12 +0100

    add toggle theme button

--- app/components/Result.js
+++ app/components/Result.js
@@ -4,6 +4,7 @@ import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from '
 import Card from './Card'
 import PropTypes from 'prop-types'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 function ProfileList ({ profile }) {
   return (
@@ -14,14 +15,18 @@ function ProfileList ({ profile }) {
       </li>
       {profile.location && (
         <li>
-          <FaCompass color='rgb(144, 115, 255)' size={22} />
-          {profile.location}
+          <Tooltip text="User's location">
+            <FaCompass color='rgb(144, 115, 255)' size={22} />
+            {profile.location}
+          </Tooltip>
         </li>
       )}
       {profile.company && (
         <li>
-          <FaBriefcase color='#795548' size={22} />
-          {profile.company}
+          <Tooltip text="User's company">
+            <FaBriefcase color='#795548' size={22} />
+            {profile.company}
+          </Tooltip>
         </li>
       )}
       <li>
@@ -40,7 +45,7 @@ ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
 }
 
-export default class Results extends React.Component {
+export default class Result extends React.Component {
   constructor(props) {
     super(props)
 
@@ -114,7 +119,7 @@ export default class Results extends React.Component {
   }
 }
 
-Results.propTypes = {
+Result.propTypes = {
   playerOne: PropTypes.string.isRequired,
   playerTwo: PropTypes.string.isRequired,
   onReset: PropTypes.func.isRequired
